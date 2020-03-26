3f719ac586131111da6deb910e1910b35d303586
Prachi Thakkar <prachi.thakkar@solutionanalysts.com>
2020-03-26 19:31:30 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import Login from './src/components/login/Login.component';
+import RecipeList from './src/components/recipelist/RecipeList.component';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import RecipeDetail from './src/components/recipedetail/RecipeDetail.component';
+import RecipeItem from './src/components/recipelist/RecipeItem.component';
+import SplashScreen from 'react-native-splash-screen';
+
+
+
+const AppNavigator = createStackNavigator(
+  {
+    Login: Login,
+    RecipeList: RecipeList,
+    RecipeItem: RecipeItem,
+    RecipeDetail : RecipeDetail
+  },
+  {
+    initialRouteName: 'Login',
+  }
+);
+
+class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+export default createAppContainer(AppNavigator);
---
+++ src/components/recipedetail/RecipeDetail.component.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import { View } from "react-native";
+import styles from './RecipeDetail.component.style'
+
+
+export default class RecipeDetail extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+                <Text>{'Prachi Thacker'}</Text>
+            </View>
+        );
+    }
+
+}
